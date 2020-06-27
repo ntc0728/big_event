f76dde32e675124dc9613bc551097759fe7026ca
nitiancheng <1343810646@qq.com>
2020-06-27 17:15:27 +0800

    完成了登录和注册的功能

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,7 +1,10 @@
 // 注意：每次调用 $.get() 或 $.post() 或 $.ajax() 的时候，
 // 会先调用 ajaxPrefilter 这个函数
 // 在这个函数中，可以拿到我们给Ajax提供的配置对象
+// ajaxPrefilter()  过滤 用户指定预先处理Ajax参数选项的额回调函数
+// const baseUrl ='http://127.0.0.1' 基准路径
+// `${baseUrl}/api/login`
 $.ajaxPrefilter(function (options) {
     // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
-    options.url = 'http://ajax.frontend.itheima.net' + options.url
+    options.url = 'http://127.0.0.1:3036' + options.url
 })
--- assets/js/login.js
+++ assets/js/login.js
@@ -11,77 +11,90 @@ $(function () {
         $('.login-box').show()
         $('.reg-box').hide()
     })
-})
 
+    // 点击 -> 触发事件 定时器  ajax  回调
 
-// 自定义校验规则
-var form = layui.form
-//通过 form.verify() 函数自定义校验规则，里面是 key：value形式，key后续对应设置到标签的 lay-verity属性中，value就是验证的规则，这里定义了两个自定义校验规则，一个是密码框，利用的是正则，一个是确认密码
-form.verify({
-    // 自定义了一个叫做 pwd 校验规则
-    pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
-    // 校验两次密码是否一致的规则
-    repwd: function (value) {
-        // 通过形参拿到的是确认密码框中的内容
-        // 还需要拿到密码框中的内容
-        // 然后进行一次等于的判断
-        // 如果判断失败,则return一个提示消息即可
-        var pwd = $('.reg-box [name=password]').val()
-        if (pwd !== value) {
-            return '两次密码不一致！'
-        }
-    }
-})
-
-var layer = layui.layer
-
-// 注册
-// 监听注册表单的提交事件
-$('#form_reg').on('submit', function (e) {
-    // 1. 阻止默认的提交行为
-    e.preventDefault()
-    // 2. 发起Ajax的POST请求
-    var data = {
-        username: $('#form_reg [name=username]').val(),
-        password: $('#form_reg [name=password]').val()
-    }
-    $.post('http://ajax.frontend.itheima.net/api/reguser', data, function (res) {
-        if (res.status !== 0) {
-            return layer.msg(res.message)
-        }
-        layer.msg('注册成功，请登录！')
-        // 模拟人的点击行为
-        $('#link_login').click()
-    })
-})
-
-//登录
-// 监听登录表单的提交事件
-$('#form_login').submit(function (e) {
-    // 阻止默认提交行为
-    e.preventDefault()
-    $.ajax({
-        url: 'http://ajax.frontend.itheima.net/api/login',
-        method: 'POST',
-        // 快速获取表单中的数据
-        data: $(this).serialize(),
-        success: function (res) {
-            if (res.status !== 0) {
-                return layer.msg('登录失败！')
+    // 自定义校验规则
+    var form = layui.form
+    //通过 form.verify() 函数自定义校验规则，里面是 key：value形式，key后续对应设置到标签的 lay-verity属性中，value就是验证的规则，这里定义了两个自定义校验规则，一个是密码框，利用的是正则，一个是确认密码
+    form.verify({
+        // 自定义了一个叫做 pwd 校验规则
+        // pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
+        pwd: function (value) {
+            const reg = /^[\S]{6,12}$/
+            if (!reg.test(value)) {
+                return '密码必须6到12位，且不能出现空格'
             }
-            layer.msg('登录成功！')
-            // 将登录成功得到的 token 字符串，保存到 localStorage 中
-            localStorage.setItem('token', res.token)
-            // 跳转到后台主页
-            location.href = '/index.html'
+        },
+        // 校验两次密码是否一致的规则
+        repwd: function (value) {
+            // 通过形参拿到的是确认密码框中的内容
+            // 还需要拿到密码框中的内容
+            // 然后进行一次等于的判断
+            // 如果判断失败,则return一个提示消息即可
+            const pwd = $('.reg-box [name=password]').val()
+            if (pwd !== value) {
+                return '两次密码不一致！'
+            }
         }
     })
+
+    var layer = layui.layer
+
+    // 注册
+    // 监听注册表单的提交事件
+    $('#form_reg').on('submit', function (e) {
+        // 1. 阻止默认的提交行为
+        e.preventDefault()
+        // 2. 发起Ajax的POST请求
+        // inputParams
+        var inputParams = {
+            username: $('#form_reg [name=username]').val(),
+            password: $('#form_reg [name=password]').val()
+        }
+        // var data = {
+        //     username: $('#form_reg [name=username]').val(),
+        //     password: $('#form_reg [name=password]').val()
+        // }
+        $.post('/api/reguser', inputParams, function (res) {
+            if (res.status !== 0) {
+                return layer.msg(res.message)
+            }
+            console.log('注册成功')
+            layer.msg('注册成功，请登录！')
+            // 模拟人的点击行为
+            $('#link_login').click()
+        })
+    })
+
+    //登录
+    // 监听登录表单的提交事件
+    $('#form_login').submit(function (e) {
+        // 阻止默认提交行为
+        e.preventDefault()
+        $.ajax({
+            url: '/api/login',
+            method: 'POST',
+            // 快速获取表单中的数据
+            //$(this).serialize() 获取表单数据 key=value&key=value
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('登录失败！')
+                }
+                layer.msg('登录成功！')
+                // 将登录成功得到的 token 字符串，保存到 localStorage 中
+                // 真正的将token落地到缓存 持久化存储的地方  
+                // 获取：localStorage.getItem('token', res.token)
+                // 清空：localStorage.removeItem('token', res.token)
+                localStorage.setItem('token', res.token)
+                // 跳转到后台主页
+                location.href = '/index.html'
+            }
+        })
+    })
+
 })
 
-// 注意：每次调用 $.get() 或 $.post() 或 $.ajax() 的时候，
-// 会先调用 ajaxPrefilter 这个函数
-// 在这个函数中，可以拿到我们给Ajax提供的配置对象
-$.ajaxPrefilter(function(options) {
-  // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
-  options.url = 'http://ajax.frontend.itheima.net' + options.url
-})
+
+
